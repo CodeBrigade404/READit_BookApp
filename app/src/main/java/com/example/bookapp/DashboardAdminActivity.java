d1b4feb16d84a02d2493e524cc49145a9e1c4429
it21016066 <it21016066@my.sliit.lk>
2022-10-22 18:00:50 +0530

    initial commit

---
+++ app/src/main/java/com/example/bookapp/DashboardAdminActivity.java
@@ -0,0 +1,66 @@
+package com.example.bookapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.bookapp.databinding.ActivityDashboardAdminBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class DashboardAdminActivity extends AppCompatActivity {
+
+    //view binding
+    private ActivityDashboardAdminBinding binding;
+
+    //firebase auth
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDashboardAdminBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+        checkUser();
+
+        //handle click, logout
+        binding.logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signOut();
+                checkUser();
+            }
+        });
+
+        //hand click,start category
+        binding.addCategoryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(DashboardAdminActivity.this, CategoryAddActivity.class));
+            }
+        });
+
+
+    }
+
+    private void checkUser() {
+        //get current user
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser==null){
+            //not logged in , go to main screen
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+        else{
+            //logged in , get user info
+            String email = firebaseUser.getEmail();
+            //set in textview of toolbar
+            binding.subTitleTv.setText(email);
+        }
+    }
+}
