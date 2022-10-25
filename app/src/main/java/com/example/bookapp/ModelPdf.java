31c291831c6425951ded262e71e2d1680f6a5386
IT21019968 <it21019968@my.sliit.lk>
2022-10-25 11:45:18 +0530

    Resolve the Pdf Viewer problem

--- app/src/main/java/com/example/bookapp/ModelPdf.java
+++ app/src/main/java/com/example/bookapp/ModelPdf.java
@@ -1,5 +1,26 @@
 package com.example.bookapp;
 
 public class ModelPdf {
-    
+ //variables
+    String uid,id,title,description,categoryId,url;
+    long timestamp;
+
+    //empty constructor
+    public ModelPdf() {
+    }
+
+    public ModelPdf(String uid, String id, String title, String description, String categoryId, String url, long timestamp) {
+        this.uid = uid;
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.categoryId = categoryId;
+        this.url = url;
+        this.timestamp = timestamp;
+    }
+
+    //-----------------Getters/Setters---------
+
+
+
 }
