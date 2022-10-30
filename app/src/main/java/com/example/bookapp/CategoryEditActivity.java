01b9da02266ff93e9e6bd6eb44367702bc481e17
Chathuka Viduranga <Chathuka.viduranga.bc@gmail.com>
2022-10-30 19:39:36 +0530

    edit category backbutton added

--- app/src/main/java/com/example/bookapp/CategoryEditActivity.java
+++ app/src/main/java/com/example/bookapp/CategoryEditActivity.java
@@ -3,12 +3,24 @@ package com.example.bookapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.bookapp.databinding.ActivityCategoryEditBinding;
+
 
 public class CategoryEditActivity extends AppCompatActivity {
+    private ActivityCategoryEditBinding activityCategoryEditBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        activityCategoryEditBinding = ActivityCategoryEditBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_category_edit);
+        setContentView(activityCategoryEditBinding.getRoot());
+        activityCategoryEditBinding.backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
